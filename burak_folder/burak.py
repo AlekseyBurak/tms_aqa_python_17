b538aba56975d2f74835b9bd0e33d7a7d3d43c70
Aliaksei Burak <Aburak@allot.com>
2024-02-06 00:05:25 +0300

    add ideal first homework
    add git notes

---
+++ burak_folder/burak.py
@@ -0,0 +1,13 @@
+print(f"Sum == {(a := int(input('Enter a '))) + (b := int(input('Enter b ')))}\nDiff == {a - b}\nProd == {a * b}")
+
+
+print(f"Result == {(abs(a := int(input('Enter a '))) - abs(b := int(input('Enter b ')))) / (1 + abs(a * b))}")
+
+
+print(f"V == {(a := int(input('Enter cube rib '))) ** 3}\n P == {4 * a ** 2}")
+
+
+print(f"Avg aryth == {((a := int(input('Enter a '))) + (b := int(input('Enter b ')))) / 2}\nAvg geo{(a * b) ** 0.5}")
+
+
+print(f"Hypo == {((a := int(input('Enter a '))) ** 2 + (b := int(input('Enter b '))) ** 2) ** 0.5}\nS == {a * b / 2}")
