b8e36bca6085e2e196dac0273797d5d380084008
Karabinevskii <159064677+Karabinevskii@users.noreply.github.com>
2024-02-05 22:26:21 +0300

    Add files via upload

---
+++ karabin/task_1_1.py
@@ -0,0 +1,8 @@
+x = float(input("Введи x: "))
+y = float(input("Введи y: "))
+z = x + y
+print("Сумма:", z)
+z1 = x - y
+print("Разность:", z1)
+z2 = x * y
+print("Произведение:", z2)
---
+++ karabin/task_1_4.py
@@ -0,0 +1,7 @@
+import math
+a = float(input("Введи a: "))
+b = float(input("Введи b: "))
+c = (a + b) / 2
+print("Среднее арифметическое:", round(c, 2))
+c1 = (math.sqrt(a * b))
+print("Среднее геометрическое:", round(c1, 2))
