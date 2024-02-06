694f88b0a28f19933759ad01d2b9dcbfa592edf0
Fedor Zmushko <fedoszmushko@gmail.com>
2024-02-06 16:34:57 +0300

    fedyazmushko

---
+++ fedyazmushko/task_1_1.py
@@ -0,0 +1,12 @@
+print('Введите своё число :)')
+
+a = int(input())
+b = int(input())
+
+sum = (a + b)
+diff = (a - b)
+comp = a * b
+
+print('Вот ваша сумма :', sum)
+print('Вот ваша разность :', diff)
+print('Вот ваше произведение :', comp)
