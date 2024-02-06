7b73e1a842dd8e87ed3abfdcfda1c42dbc58f519
viktoria hardzei <viktoria_gordey@mail.ru>
2024-02-06 21:42:14 +0300

    hw

---
+++ hard-folder/1.py
@@ -0,0 +1,4 @@
+a = int(input("Введите числа:\n"))
+b = int(input())
+print(a + b, a - b, a * b, sep='\n')
+
---
+++ hard-folder/4.py
@@ -0,0 +1,3 @@
+a = int(input("Введите числа:\n"))
+b = int(input())
+print((a + b) / 2, (a * b)**(1 / 2), sep='\n')
