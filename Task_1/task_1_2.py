6da8b6fdc3083a515cca5d0f2fa7bdb5acccadf7
alesia-dirina <159064858+alesia-dirina@users.noreply.github.com>
2024-02-07 14:21:34 +0300

    Delete Task_1 directory

--- Task_1/task_1_2.py
+++
@@ -1,9 +0,0 @@
-import math
-
-a = int(input("Введи число x: "))
-b = int(input("Введи число y: "))
-
-print("|x| - |y|")
-print("--------- =",
-      (math.fabs(a) - math.fabs(b)) / (1 + math.fabs(a*b)))
-print(" 1 + |xy|")
