a607d19174b7c4f84e8af9089512f9dc59b50ca4
annayadevich <annayadevich@gmail.com>
2024-02-08 21:54:04 +0300

    homework 1

---
+++ annayadevich/lesson3.py
@@ -0,0 +1,44 @@
+# number_of_dogs = 1
+# # string
+# # """"""
+# # iterable
+# # """"""
+#
+#
+# a = "string"
+# a = a[0] +'T' + a[2:]
+# print(a[0])
+# a = "sTring"
+# print(a[0])
+# b = "another string"
+# c = """""
+# new
+# name
+# """""
+# print(c)
+#
+# name = "anna\n"
+# surname = "yadevich"
+# print(name *3)
+#
+# a = "new"
+# print(a.index("w"))
+#
+# a = "nekjgkgkWnknkkbWkbkbbW"
+# print(a.split(sep= "W"))
+#
+# # TODO Add usefull examples
+#
+# a = 0
+# # some code
+# # if da:
+# a = a + 1
+# print(a)
+#
+# a: int = 1
+# print(a)
+#
+
+a = input("Введите глагол: ")
+ing_form = a + "ing"
+print(f"Инговая форма глагола '{a}' - это '{ing_form}'.")
