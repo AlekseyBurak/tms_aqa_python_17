c1b9908f913c52ab671b7a74a9b3368095d2555b
AlekseyBurak <burakaleksey@gmail.com>
2024-02-08 19:29:34 +0300

    first commit lesson 3

---
+++ burak_folder/lesson_3/data_types.py
@@ -0,0 +1,19 @@
+# string
+
+s = r'C:\some\path'
+# index
+# rindex
+# find
+# rfind
+
+
+# a = '12344'
+# print(a.replace("4", "5", 1))
+#
+# def myFunc(e):
+#   return len(e)
+#
+# cars = ['Ford', 'Mitsubishi', 'BMW', 'VW']
+#
+# cars.sort(key=myFunc)
+# print(cars)
