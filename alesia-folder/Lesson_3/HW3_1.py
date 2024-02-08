9fe3e935226c7146578978af9d592e84258dcd79
alesia.dirina <alesia.dirina.bbv@gmail.com>
2024-02-08 21:55:53 +0300

    wow!

--- alesia-folder/Lesson_3/HW3_1.py
+++ alesia-folder/Lesson_3/HW3_1.py
@@ -1,3 +1,18 @@
 a = "do"
 b = "ing"
-print(a + b)
+print("do + ing = ", a + b)
+c = input("Type a verb: ")
+print(c, "ing", sep="")
+print(c + "ing")
+
+d = 10
+e = True
+
+print(f"Переменная {d} это то же самое, что и {e}")
+print(f"Переменная {d} это то же самое, что и {'True' if e else 'False'}")
+
+# TODO print("ку-ку")
+
+"""
+This is how it works.
+"""
