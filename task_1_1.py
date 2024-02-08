87c3523c4667c7e8678d7f08021cc8ba49a0e035
Karabinevskii Alexandr <karabinevski@mail.ru>
2024-02-08 22:19:03 +0300

    new file for work

--- task_1_1.py
+++
@@ -1,8 +0,0 @@
-x = float(input("Введи x: "))
-y = float(input("Введи y: "))
-z = x + y
-print("Сумма:", z)
-z1 = x - y
-print("Разность:", z1)
-z2 = x * y
-print("Произведение:", z2)
--- test.py
+++
@@ -1,4 +0,0 @@
-print("Hello Git")
-# print("My name is Alexandr")
-# print("My last name is Karabevskii")
-# print("I was born in 1984")
