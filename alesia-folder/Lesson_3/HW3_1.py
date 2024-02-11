8422dc5c03daf926f18a1916ee8bdd491f6f3854
alesia.dirina <alesia.dirina.bbv@gmail.com>
2024-02-11 13:17:52 +0300

    commit of homework 3

--- alesia-folder/Lesson_3/HW3_1.py
+++ alesia-folder/Lesson_3/HW3_1.py
@@ -1,18 +1,4 @@
-a = "do"
-b = "ing"
-print("do + ing = ", a + b)
-c = input("Type a verb: ")
-print(c, "ing", sep="")
-print(c + "ing")
-
-d = 10
-e = True
-
-print(f"Переменная {d} это то же самое, что и {e}")
-print(f"Переменная {d} это то же самое, что и {'True' if e else 'False'}")
-
-# TODO print("ку-ку")
-
-"""
-This is how it works.
-"""
+print("Замена символа в строке")
+f = "www.my_site.com#about"
+print("Было: ", f, "\nСтало: ",
+      f.replace("#", "/"))
