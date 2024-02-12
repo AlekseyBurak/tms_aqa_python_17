e894545a547251364e1006c968b04a64c6728822
Karabinevskii Alexandr <karabinevski@mail.ru>
2024-02-12 22:09:28 +0300

    cw_4

--- karabin/cw_4/cw_4.py
+++ karabin/cw_4/cw_4.py
@@ -1,9 +1,19 @@
-# l = [1, 21342, "text", 0.9, .9, 1_000_000, None, True, False, [1, 2], (1, 4)]
-# l1 = list("text")
-# print(l)
-# print(l1)
+# # l = [1, 21342, "text", 0.9, .9, 1_000_000, None, True, False, [1, 2], (1, 4)]
+# # l1 = list("text")
+# # print(l)
+# # print(l1)
+#
+# def foo(n: int)
+#     print(n ** 2)
+#
+# def bar(n: int)
+#
+# a = [1, 2, 3]
+# b = [1, 2, 3]
 
-def foo(n: int)
-    print(n ** 2)
-
-def bar(n: int)
+l = [1, 2, 3, 3, 4, 5]
+l.append(16)
+l.append("text")
+l.append([1, 2, 3])
+l.append(None)
+print(l)
