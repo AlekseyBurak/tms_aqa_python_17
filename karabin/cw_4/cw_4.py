f1369f024d58e6159a96587de0cf6176518417e5
Karabinevskii Alexandr <karabinevski@mail.ru>
2024-02-12 19:29:14 +0300

    New hw

---
+++ karabin/cw_4/cw_4.py
@@ -0,0 +1,9 @@
+# l = [1, 21342, "text", 0.9, .9, 1_000_000, None, True, False, [1, 2], (1, 4)]
+# l1 = list("text")
+# print(l)
+# print(l1)
+
+def foo(n: int)
+    print(n ** 2)
+
+def bar(n: int)
