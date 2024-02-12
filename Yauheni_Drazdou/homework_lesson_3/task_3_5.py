56127990b9535b8fba490ed3e4222e1d27aacdf3
Yauheni <genodrazdou@gmail.com>
2024-02-12 16:55:39 +0700

    task 3

--- Yauheni_Drazdou/homework_lesson_3/task_3_5.py
+++ Yauheni_Drazdou/homework_lesson_3/task_3_5.py
@@ -1,4 +1,7 @@
-# ASCII symbol into number
+import math
+# ASCII symbol into number and task 6
 print("Gimme a ACSII symbol")
 s = str(input())
-print(ord(s))
+t = ord(s)
+print(f"Here is the number that matches {s}\n {t}\n А вот результат "
+      f"деления корня на длину числа\n {math.sqrt(t)/len(str(t))}")
