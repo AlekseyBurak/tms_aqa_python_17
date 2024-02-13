36e7a7293c15451bc65322453f5c14792a777dc4
Karabinevskii Alexandr <karabinevski@mail.ru>
2024-02-13 19:08:51 +0300

    change directory and new homework

---
+++ karabin/homework/homework_4/homework_5.py
@@ -0,0 +1,17 @@
+# string = input("Enter your sentence\n: ")
+# n_string = string.split()
+# print(n_string)
+
+# l = ['Aleksandr', 'Karabinevski']
+# a = "Minsk"
+# c = "Belarus"
+# print(f"Привет {(" ".join(l))}! Добро пожаловать в {a} {c}")
+
+# list = ["I", "love", "arrays", "they", "are", "my", "favorite"]
+# print(" ".join(list))
+
+# list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# list[3] = 11
+# deleted_number = list.pop(6)
+# print(list)
+
