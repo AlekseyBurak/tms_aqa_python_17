d4173878404908df89c8146ffb31f4aff5130a4c
Yauheni <genodrazdou@gmail.com>
2024-02-13 13:55:32 +0700

    task 3

--- Yauheni_Drazdou/homework_lesson_4/task_4_3.py
+++ Yauheni_Drazdou/homework_lesson_4/task_4_3.py
@@ -1,5 +1,4 @@
 # Дан список ["I", "love", "arrays", "they", "are", "my", "favorite"]
 # сделайте из него строку => "I love arrays they are my favorite"
 l =["I", "love", "arrays", "they", "are", "my", "favorite"]
-s = str(l)
-print(s)
+print(" ".join(l))
