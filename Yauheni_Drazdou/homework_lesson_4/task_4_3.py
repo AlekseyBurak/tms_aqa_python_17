a827309b0f765121484945d9bbc35eb32cc1c62d
Yauheni <genodrazdou@gmail.com>
2024-02-13 13:46:54 +0700

    task 3

---
+++ Yauheni_Drazdou/homework_lesson_4/task_4_3.py
@@ -0,0 +1,5 @@
+# Дан список ["I", "love", "arrays", "they", "are", "my", "favorite"]
+# сделайте из него строку => "I love arrays they are my favorite"
+l =["I", "love", "arrays", "they", "are", "my", "favorite"]
+s = str(l)
+print(s)
---
+++ Yauheni_Drazdou/lesson_4/lesson_4.py
@@ -0,0 +1,19 @@
+l = [1, 2134, "text", None, True, False, [1,2], [1, 4], {1: "a"}]
+text = "text"
+l1 = list("text")
+print(l)
+
+print(l1)
+
+print(l[::-1])
+l.append(16)
+l.remove(2134)
+
+print(l.sort)
+
+
+a = [1, 2, 3]
+b = [1, 2, 3]
+print(a == b)
+print(a is b)
+print(True is True)
