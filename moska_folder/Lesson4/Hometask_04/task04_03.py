e8764282b16938437736d2c71b9cd587d51fbdd1
nastassia.moska@globant.com <nastassia.moska@globant.com>
2024-02-13 18:05:54 +0300

    hometasks_4_Moska

---
+++ moska_folder/Lesson4/Hometask_04/task04_03.py
@@ -0,0 +1,5 @@
+# 3) Дан список ["I", "love", "arrays", "they", "are", "my", "favorite"] сделайте из него строку
+# => "I love arrays they are my favorite"
+
+a = ["I", "love", "arrays", "they", "are", "my", "favorite"]
+print(' '.join(a))
