93e8fa41ae86a345c0fedd4c789a109772670422
Yauheni <genodrazdou@gmail.com>
2024-02-14 23:15:04 +0700

    task 3

--- Yauheni_Drazdou/homework_lesson_4/task_4_5.py
+++ Yauheni_Drazdou/homework_lesson_4/task_4_5.py
@@ -1,5 +1,4 @@
-import _collections
-from _collections import _count_elements
-l= [1, 2, 3, 2, 1]
-print(_collections._cou)
-
+l = [1, 2, 3, 2, 1]
+for i in l:
+    if l.count(i) == 1:
+        print(i)
