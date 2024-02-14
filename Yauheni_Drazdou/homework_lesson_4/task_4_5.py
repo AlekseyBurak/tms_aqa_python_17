7c167dd32b9cbd9df20f8c9b9e76c258781b4b10
Yauheni <genodrazdou@gmail.com>
2024-02-14 20:54:04 +0700

    task 3

--- Yauheni_Drazdou/homework_lesson_4/task_4_5.py
+++ Yauheni_Drazdou/homework_lesson_4/task_4_5.py
@@ -1,10 +1,5 @@
-
-m = [2, 1, 2]
-l = len(m)
-x = l - 1
-#if not m[(l-(x+1))] == m[(l-x):(x-l)]:
-    #print(m[(l-(x+1))])
-
-
-
+import _collections
+from _collections import _count_elements
+l= [1, 2, 3, 2, 1]
+print(_collections._cou)
 
