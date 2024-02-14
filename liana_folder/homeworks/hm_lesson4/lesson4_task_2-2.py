acc0ce4b8b204a2e58fcf3f787fbc36764244601
LianaKarpeikaa <ulyana.karpeika@gmail.com>
2024-02-14 13:44:13 +0100

    liana's homework 4

---
+++ liana_folder/homeworks/hm_lesson4/lesson4_task_2-2.py
@@ -0,0 +1,5 @@
+name = input("Enter First and Last name: ")
+lst = name.split()
+a = input("Enter City: ")
+c = input("Enter Country: ")
+print(f"Привет, {lst[0]} {lst[1]}! Добро пожаловать в {a} {c}")
---
+++ liana_folder/homeworks/hm_lesson4/lesson4_task_2.py
@@ -0,0 +1,4 @@
+lst = ['Ivan', 'Ivanou']
+a = 'Minsk'
+c = 'Belarus'
+print(f"Привет, {lst[0]} {lst[1]}! Добро пожаловать в {a} {c}")
