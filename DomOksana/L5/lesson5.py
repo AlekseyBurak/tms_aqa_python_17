20a653022fdb15dba8b1bf83bc95c86052cd6f3a
Oksana Domanskaya <okej31@mail.ru>
2024-02-15 20:42:04 +0300

    1 часть урока 5

---
+++ DomOksana/L5/lesson5.py
@@ -0,0 +1,37 @@
+import datetime
+import random
+import time
+# делаем список из цифр
+# list_integer = [i for i in range(1,11)]
+# print(list_integer)
+#
+# print(list(range(5,51,5)))
+
+# делаем список из строк
+# list_integer = [str(i) for i in range(1,11)]
+# print(list_integer)
+#
+#
+# list_integer = [i for i in range(1,11)]
+# list_string = map (str, list_integer)
+# print(type(list_string))
+
+list_people = [f"Human{index}" for index in range(1, 11)]
+list_age = [random.randint(18, 45) for _ in range(10)]
+
+
+zip_obj = zip(list_people, list_age)
+print(zip_obj)
+
+print(list(zip_obj))
+
+dict_from_zip = {}
+
+for key, value in list (zip_obj):
+    dict_from_zip[key] = value
+print()
+print(dict_from_zip)
+dict_from_zip.update (zip_obj)
+print(dict_from_zip)
+
+
