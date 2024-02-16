bcc5e27cefef787073a6e119e5a9d79b8a7b53c8
i-turbal <i.turbal86@gmail.com>
2024-02-16 22:16:40 +0300

    программа выдает по карте, если ответ да

--- i_turbal_filed/HW5/black_jack2.py
+++ i_turbal_filed/HW5/black_jack2.py
@@ -1,21 +1,24 @@
+# Суть: Я раздаю диллеру и игроку по две карту, одну ссверху и одну снизу, что не имеет значение
+# Карты диллера не показываю
+# После создаю цикл while True (так нельзя, но я не придумал как по другому :))
+# если на вопрос ответ yes, то добавляется карта диллеру и игроку, обнуляется пред. очки и считаются заново.
+# если ответ !yes, то выходим с цикла
+# после сверяем результаты
+# про тузы когда после 21 не делал, возможно к этому вопросу вернусть позже
+
 import random
 
 # создаем значения карты
 cards = [str(i) for i in range(1, 11)] + ['J', 'Q', 'K', 'A']
-# print(cards)
-# create suit cards
 suits = ("Diamonds", "Hearts", "Clubs", "Spades",)
 all_cards = []
 for card in cards:
     for suit in suits:
         all_cards.append(f"{card} - {suit}")
-# print(all_cards)
-# create scopes for cards
 
 #mix cards:
 for _ in range(5):
     random.shuffle(all_cards)
-# print(all_cards)
 # create scope for cards
 
 scores = {}
@@ -26,7 +29,6 @@ for card in cards:
         scores[card] = 10
     else:
         scores[card] = 11
-# print(scores)
 
 # создаем раздачу
 
@@ -34,9 +36,7 @@ dealer_hand = []
 for _ in range(1):
     dealer_hand.append(all_cards.pop())
     dealer_hand.append(all_cards.pop(-1))
-# print(dealer_hand)
-#
-# print(all_cards)
+
 
 player_hand = []
 for _ in range(1):
@@ -52,8 +52,7 @@ for card in dealer_hand:
         dealer_score += 10
     else:
         dealer_score += scores[card[0]]
-print(dealer_hand)
-print(dealer_score)
+
 
 player_score = 0
 for card in player_hand:
@@ -61,33 +60,56 @@ for card in player_hand:
         player_score += 10
     else:
         player_score += scores[card[0]]
-print(player_hand)
-print(player_score)
+print(f"Your cards is {player_hand}\n and your scope is {player_score}")
 
-question = input("Do you want to add card")
+
 while True:
+    question = input("Do you want to add card")
+    # добавляем карту диллеру
     if question == "yes":
         for _ in range(1):
-            dealer_hand.append(all_cards.pop(random.randrange(0, len(all_cards))))
+            dealer_hand.append(all_cards.pop(-1))
+        dealer_score = 0
         for card in dealer_hand:
             if card.startswith("10"):
                 dealer_score += 10
             else:
                 dealer_score += scores[card[0]]
+
+
         for _ in range(1):
             for _ in range(1):
-                player_hand.append(all_cards.pop(random.randrange(0, len(all_cards))))
-        print(dealer_hand)
+                player_hand.append(all_cards.pop(-1))
+        player_score = 0
         for card in player_hand:
             if card.startswith("10"):
                 player_score += 10
             else:
                 player_score += scores[card[0]]
-        break
+        print(f"Your cards is {player_hand}\n and your scope is {player_score}")
     elif question == "no":
         break
 
 
 
+# проверка результата
+if player_score > 21 and dealer_score > 21:
+    if player_score < dealer_score:
+        print(f" You have {player_score}\n delaer has {dealer_score}\n You win!")
+    elif player_hand > dealer_hand:
+        print(f" You have {player_score}\n delaer has {dealer_score}\n You lose!")
+    elif player_hand == dealer_hand:
+        print(f" You have {player_score}\n delaer has {dealer_score}\n You and dealer has the same points!")
+if player_score < 21 and dealer_score < 21:
+    if player_score < dealer_score:
+        print(f" You have {player_score}\n delaer has {dealer_score}\n You lose!")
+    elif player_hand > dealer_hand:
+        print(f" You have {player_score}\n delaer has {dealer_score}\n You win!")
+    elif player_hand == dealer_hand:
+        print(f" You have {player_score}\n delaer has {dealer_score}\n You and dealer has the same points!")
+if player_score < 21 and dealer_score > 21:
+    print(f" You have {player_score}\n delaer has {dealer_score}\n You win!")
+if player_score > 21 and dealer_score < 21:
+    print(f" You have {player_score}\n delaer has {dealer_score}\n You lose!")
 
 
