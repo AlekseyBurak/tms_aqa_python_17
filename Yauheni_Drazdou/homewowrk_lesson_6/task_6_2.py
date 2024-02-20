b0fde0bed3ed857bc6bf8461d6e48665ca8f88e0
Yauheni <genodrazdou@gmail.com>
2024-02-20 15:49:36 +0700

    task 6_2

--- Yauheni_Drazdou/homewowrk_lesson_6/task_6_2.py
+++ Yauheni_Drazdou/homewowrk_lesson_6/task_6_2.py
@@ -1,11 +1,13 @@
 def fun(*args):
     print("Введите операцию:\n1. Сложение\n2. Вычитание\n3. Умножение\n4. Деление\n5. Выход")
     move = int(input())
+
     while move < 5:
         print("Введите первое число")
         a = int(input())
         print("Введите второе число")
         b = int(input())
+
         if move == 1:
             print("Сумма")
             return a + b
@@ -18,4 +20,5 @@ def fun(*args):
         if move == 4:
             print("Частное")
             return a / b
+
 print(fun())
