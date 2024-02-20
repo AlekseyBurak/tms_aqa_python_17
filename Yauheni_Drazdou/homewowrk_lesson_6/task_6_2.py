df9e4c3fe9c050d88f5f3e01eacaccb54f127ea4
Yauheni <genodrazdou@gmail.com>
2024-02-20 16:37:26 +0700

    функция задачи 2

--- Yauheni_Drazdou/homewowrk_lesson_6/task_6_2.py
+++ Yauheni_Drazdou/homewowrk_lesson_6/task_6_2.py
@@ -1,24 +1,28 @@
 def fun(*args):
-    print("Введите операцию:\n1. Сложение\n2. Вычитание\n3. Умножение\n4. Деление\n5. Выход")
-    move = int(input())
-
-    while move < 5:
+    while True:
+        print("Введите операцию:\n1-Сложение  2-Вычитание  3-Умножение  4-Деление  5-Выход")
+        move = int(input())
+        if move == 5:
+            break
         print("Введите первое число")
         a = int(input())
         print("Введите второе число")
         b = int(input())
 
         if move == 1:
-            print("Сумма")
-            return a + b
+            print(f"Сумма {a + b}")
+
         if move == 2:
-            print("Разность")
-            return a - b
+            print(f"Разность {a - b}")
+
         if move == 3:
-            print("Произведение")
-            return a * b
+            print(f"Произведение {a * b}")
+
         if move == 4:
-            print("Частное")
-            return a / b
+            print(f"Частное {a / b}")
+
 
 print(fun())
+
+
+
