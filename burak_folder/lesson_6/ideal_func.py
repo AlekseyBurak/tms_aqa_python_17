e56c6943407c89adabe40e8a1904488db8b85222
AlekseyBurak <burakaleksey@gmail.com>
2024-02-20 08:21:50 +0300

    ideal func example and some advanced arguments stuff

---
+++ burak_folder/lesson_6/ideal_func.py
@@ -0,0 +1,10 @@
+
+def func(name: str) -> str:
+    """
+    This func will greet you in person
+
+    :param name: your name
+    :return: str
+    """
+    greeting = f"Hello {name}"
+    return greeting
