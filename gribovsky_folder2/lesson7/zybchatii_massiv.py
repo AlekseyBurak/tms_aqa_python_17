434bc9ee2798318098e8218b9b991aa09e5c9e4c
gribovsky <m.gribovsky@gmail.com>
2024-02-25 22:36:24 +0300

    HW for 7th lesson (зубчатый массив, работает, если последнее число двухзначное)

--- gribovsky_folder2/lesson7/zybchatii_massiv.py
+++ gribovsky_folder2/lesson7/zybchatii_massiv.py
@@ -1,6 +1,9 @@
 x = [1, 2, [3, 4, [5, 6], 7], [8, [9, [1011]]]]
 lst = []
 map_str = [str(i) for i in x]
+x = [1, 2, [3, 4, [5, 6], 7], [8, [9, [10]]]]
+lst = []
+map_str = [str(i) for i in x]
 print(map_str)
 for i in map_str:
     if len(i) == 1:
@@ -8,14 +11,13 @@ for i in map_str:
     # print(lst)
     else:
         a = [str(j) for j in i]
-        print(a)
-        for ind in range(len(a)):
-            if a[ind].isdigit():
-                b = ''
-                b += a[ind]
-                lst.append(b)
-                break
-            if a[ind].isdigit() and not a[ind+1].isdigit() and not a[ind-1].isdigit():
+        # print(a)
+        for ind in range(len(a) - 1):
+
+            if a[ind].isdigit() and not a[ind + 1].isdigit() and not a[ind - 1].isdigit():
                 lst.append(a[ind])
+
+            elif a[ind].isdigit() and a[ind + 1].isdigit():  # работает, если последнее число двухзначное
+                lst.append(a[ind] + a[ind+1])
 
 print(lst)
