3e2d7b612901182196c791e087c8c9c2a7138756
LianaKarpeikaa <ulyana.karpeika@gmail.com>
2024-02-26 17:25:28 +0300

    liana's lesson7_notes.py

---
+++ liana_folder/lesson7/lesson7_notes.py
@@ -0,0 +1,21 @@
+# def func1() -> int:
+#     while True:
+#         number = (input())
+#         if int(number) < 0:
+#             print(f"Number {number} is - Try again")
+#             return False
+#         else:
+#             lst_number = list(str(number))
+#             lst_number.reverse()
+#             reverse_int = int("".join(lst_number))
+#             print(reverse_int)
+#             return reverse_int
+#
+# func1()
+
+
+# my_list = ["dbkpodrkp", "riojgeiojg", "rg", "rggrg"]
+# my_str = filter(lambda x: len(x) < 5, my_list)
+# print(list(my_str))
+
+# grow_list()
