444ffc64e3e7f32f2f20b31baa1c39803b56426b
annayadevich <annayadevich@gmail.com>
2024-03-03 21:26:37 +0300

    Homework 9

---
+++ annayadevich/Lesson 9/HW9.py
@@ -0,0 +1,13 @@
+
+import time
+class Room:
+    height_room = float(input("Enter height:")
+    width_room = float(input("Enter width:")
+    depth_room = float(input("Enter depth:")
+
+    def floor_area(self):
+        door_width = float(input("Enter width:")
+        door_height = float(input("Enter height:")
+
+
+
