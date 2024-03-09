1706c2d4ab41403a476ea626f26ee04f4f026233
Yana Pykhtsenko <yana.pykhtsenko@gmail.com>
2024-03-09 20:13:56 +0300

    Домашка

--- pykhtsenko/Lesson_11/Homework_11.py
+++ pykhtsenko/Lesson_11/Homework_11.py
@@ -3,4 +3,4 @@ def write_to_file(file_name, phrase, count):
         for _ in range(count):
             file.write(phrase + '\n')
 
-write_to_file('text.txt', 'Я не буду лгать!', 25)
+write_to_file('text.txt', 'Я не буду лгать!!!', 25)
