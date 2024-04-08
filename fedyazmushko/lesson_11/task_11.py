00138b6be438d77f1223e3bb325b834bf60c55d1
Fedor Zmushko <fedoszmushko@gmail.com>
2024-04-08 22:09:52 +0300

    fedyazmushko

--- fedyazmushko/lesson_11/task_11.py
+++
@@ -1,6 +0,0 @@
-def write_to_file(file_name, phrase, times):
-    with open(file_name, 'w') as file:
-        for _ in range(times):
-            file.write(phrase + '\n')
-
-write_to_file('output.txt', 'Я не буду лгать', 5)
