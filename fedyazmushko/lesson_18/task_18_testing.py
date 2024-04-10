c4456d7fc94024c2a19cfc5f7ecd794b397e33f5
Fedor Zmushko <fedoszmushko@gmail.com>
2024-04-10 16:36:10 +0300

    fedyazmushko

---
+++ fedyazmushko/lesson_18/task_18_testing.py
@@ -0,0 +1,49 @@
+import unittest
+
+
+class Calc:
+    def calc(self, a: int, b: int, operation: str):
+        if operation == "+":
+            return a + b
+        elif operation == "-":
+            return a - b
+        elif operation == "*":
+            return a * b
+        elif operation == "/":
+            return a / b
+        else:
+            return "Error"
+
+
+class TestApplication(unittest.TestCase):
+
+    def setUp(self):
+        print()
+        print("i'm setUp")
+        self.calc = Calc()
+
+    def tearDown(self):
+        print()
+        print("i'm tearDown")
+
+
+    def test_1(self):
+        self.assertEqual(self.calc.calc(6, 2, "+"), 8)
+
+    def test_2(self):
+        self.assertEqual(self.calc.calc(6, 2, "-"), 4)
+
+    def test_3(self):
+        self.assertEqual(self.calc.calc(6, 2, "*"), 12)
+
+    def test_4(self):
+        self.assertEqual(self.calc.calc(6, 2, "/"), 3)
+
+    def test_5(self):
+        self.assertIsInstance(self.calc.calc(6, 2, "+"), (int, str))
+
+    # def test_6(self):
+    #     with self.assertRaises(TypeError):
+    #         self.calc.calc(6, 2, 54)
+    # С этим немного не разобрался)
+
