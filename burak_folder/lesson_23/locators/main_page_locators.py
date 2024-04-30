24f8a7e3f204dfec2e24b3cb4bdb0b534d4c3748
AlekseyBurak <burakaleksey@gmail.com>
2024-04-30 16:34:46 +0300

    hw 23 and cw 24

--- burak_folder/lesson_23/locators/main_page_locators.py
+++ burak_folder/lesson_23/locators/main_page_locators.py
@@ -8,4 +8,6 @@ IN_SEARCH_FIELD = By.XPATH, '//input[@class="fast-search__input"]'
 IFRAME = By.XPATH, '//iframe[@class="modal-iframe"]'
 
 
-LI_SEARCH_RESULT = By.XPATH, '//li[@class="search__result"]'
+LI_SEARCH_RESULT_FOR_INDEX = By.XPATH, '(//li[contains(@class, "search__result")])'
+
+BN_CATALOG = By.XPATH, '(//a[@href="https://catalog.onliner.by"])[2]'
--- burak_folder/lesson_23/pages/main_page.py
+++ burak_folder/lesson_23/pages/main_page.py
@@ -1,4 +1,7 @@
-from burak_folder.lesson_23.locators.main_page_locators import CURR_INFO, IN_SEARCH_FIELD, IFRAME, LI_SEARCH_RESULT
+from selenium.webdriver.common.by import By
+
+from burak_folder.lesson_23.locators.main_page_locators import CURR_INFO, IN_SEARCH_FIELD, IFRAME, \
+    LI_SEARCH_RESULT_FOR_INDEX, BN_CATALOG
 from burak_folder.lesson_23.pages.base_page import BasePage
 
 
@@ -14,7 +17,7 @@ class MainPage(BasePage):
 
     @property
     def search_result(self):
-        return self.driver.find_element(*LI_SEARCH_RESULT)
+        return self.driver.find_element(*LI_SEARCH_RESULT_FOR_INDEX)
 
     def get_current_currency_rate(self):
         return self.text(CURR_INFO)
@@ -24,14 +27,19 @@ class MainPage(BasePage):
 
     def input_into_search_field(self, text: str):
         self.search_field.send_keys(text)
+        self.driver.find_element(*IN_SEARCH_FIELD).send_keys(text)
 
-    def chose_from_iframe_by_index(self):
+    def chose_from_iframe_by_index(self, index: int):
         iframe = self.wait_for(IFRAME)
         self.driver.switch_to.frame(iframe)
-        self.wait_for(LI_SEARCH_RESULT)
-        self.click(LI_SEARCH_RESULT)
+        locator = By.XPATH, f'(//li[contains(@class, "search__result")])[{index}]'
+        self.wait_for(locator)
+        self.click(locator)
         self.driver.switch_to.default_content()
 
     def check_dollar_rate_visible(self):
         rate = self.get_current_currency_rate()
         assert "$" in rate, f"Description, this test failed because of\n{rate=}"
+
+    def click_bn_catalog(self):
+        self.click(BN_CATALOG)
